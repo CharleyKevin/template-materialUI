d6bd71253cbd3831f59b80e23220dda3b83712f2
Charley Kevin <charleyk05@gmail.com>
2023-11-26 14:43:02 -0300

    add custom login

--- src/components/header.js
+++ src/components/header.js
@@ -7,29 +7,35 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 //import MenuIcon from '@mui/icons-material/Menu';
  
-function Header() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              {/*  
-              <MenuIcon />
-              */}
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Gerenciamento de Projetos
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    );
+function Header({ onLogout, isAuthenticated }) {
+  const handleLogout = () => {
+    onLogout();
+  };
+
+
+  return (
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/*  
+            <MenuIcon />
+            */}
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Gerenciamento de Projetos
+          </Typography>
+          {isAuthenticated && <Button color="inherit" onClick={handleLogout}>Sair</Button>}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
  
 export default Header;
